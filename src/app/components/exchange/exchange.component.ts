bc0981146b408553cc902cb1d67655a28017fca6
Kirill Skomorokhov <kiruxa.37v@gmail.com>
2025-05-25 14:58:32 +0300

    Create Exchange page Add component chat-list Add component chat Add component search-bar Exchange consist of chat | chat-list | search-bar

--- src/app/components/exchange/exchange.component.ts
+++ src/app/components/exchange/exchange.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { ChatListComponent } from '../chat-list/chat-list.component';
+import {ChatComponent} from '../chat/chat.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-exchange',
+  imports: [ChatListComponent, ChatComponent, SearchBarComponent],
   templateUrl: './exchange.component.html',
   styleUrls: ['./exchange.component.css'],
   standalone: true
